0b15245c4cea99eeb4d1331a76e0f4dc6f885390
Christoph Martenka <christoph.martenka@yahoo.de>
2023-11-25 12:18:52 +0100

    :sparkles: Add day template

---
+++ advent-of-code-2023/day0/Test.cs
@@ -0,0 +1,32 @@
+﻿using advent.of.code.util;
+using NUnit.Framework;
+
+namespace advent.of.code.day0; 
+
+[TestFixture]
+internal class Test {
+
+    [Test]
+    public void Task1_TestInput() {
+        var result = Solution.Task1(FileReader.GetFileForDay(0, "testInput.txt"));
+        Assert.That(result == 0);
+    }
+
+    [Test]
+    public void Task1_RealInput() {
+        var result = Solution.Task1(FileReader.GetInputForDay(0));
+        Assert.That(result == 0);
+    }
+
+    [Test]
+    public void Task2_TestInput() {
+        var result = Solution.Task2(FileReader.GetFileForDay(0, "testInput.txt"));
+        Assert.That(result == 0);
+    }
+
+    [Test]
+    public void Task2_RealInput() {
+        var result = Solution.Task2(FileReader.GetInputForDay(0));
+        Assert.That(result == 0);
+    }
+}
