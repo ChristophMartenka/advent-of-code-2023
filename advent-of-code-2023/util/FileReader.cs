33ddd06f0ba97a2102d41d17f01e3ca78ef2cb88
Christoph Martenka <christoph.martenka@yahoo.de>
2023-11-25 12:17:26 +0100

    :sparkles: Add FileReader util class

---
+++ advent-of-code-2023/util/FileReader.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace advent.of.code.util; 
+
+public static class FileReader {
+    
+    private static StreamReader GetFile(string fileNamespace, string fileName) {
+        var file = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{fileNamespace}.{fileName}");
+        return new StreamReader(file);
+    }
+
+    public static StreamReader GetFileForDay(int day, string fileName) {
+        return GetFile($"advent.of.code.day{day}", fileName);
+    }
+
+    public static StreamReader GetInputForDay(int day) {
+        return GetFileForDay(day, "input.txt");
+    }
+}
