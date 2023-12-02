a9bcad965dbacbb044eb9ed563d60aeda427352f
Christoph Martenka <christoph.martenka@yahoo.de>
2023-12-02 09:56:15 +0100

    :recycle: Use expressive assert for mismatches

--- advent-of-code-2023/day0/Test.cs
+++ advent-of-code-2023/day0/Test.cs
@@ -1,7 +1,7 @@
 ﻿using advent.of.code.util;
 using NUnit.Framework;
 
-namespace advent.of.code.day0; 
+namespace advent.of.code.day0;
 
 [TestFixture]
 internal class Test {
@@ -9,24 +9,24 @@ internal class Test {
     [Test]
     public void Task1_TestInput() {
         var result = Solution.Task1(FileReader.GetFileForDay(0, "testInput.txt"));
-        Assert.That(result == 0);
+        Assert.AreEqual(result, 0);
     }
 
     [Test]
     public void Task1_RealInput() {
         var result = Solution.Task1(FileReader.GetInputForDay(0));
-        Assert.That(result == 0);
+        Assert.AreEqual(result, 0);
     }
 
     [Test]
     public void Task2_TestInput() {
         var result = Solution.Task2(FileReader.GetFileForDay(0, "testInput.txt"));
-        Assert.That(result == 0);
+        Assert.AreEqual(result, 0);
     }
 
     [Test]
     public void Task2_RealInput() {
         var result = Solution.Task2(FileReader.GetInputForDay(0));
-        Assert.That(result == 0);
+        Assert.AreEqual(result, 0);
     }
 }
--- advent-of-code-2023/day1/Test.cs
+++ advent-of-code-2023/day1/Test.cs
@@ -1,7 +1,7 @@
 ﻿using advent.of.code.util;
 using NUnit.Framework;
 
-namespace advent.of.code.day1; 
+namespace advent.of.code.day1;
 
 [TestFixture]
 internal class Test {
@@ -9,28 +9,24 @@ internal class Test {
     [Test]
     public void Task1_TestInput() {
         var result = Solution.Task1(FileReader.GetFileForDay(1, "testInput.txt"));
-        Console.Out.WriteLine(result);
-        Assert.That(result == 142);
+        Assert.AreEqual(result, 142);
     }
 
     [Test]
     public void Task1_RealInput() {
         var result = Solution.Task1(FileReader.GetInputForDay(1));
-        Console.Out.WriteLine(result);
-        Assert.That(result == 54708);
+        Assert.AreEqual(result, 54708);
     }
 
     [Test]
     public void Task2_TestInput() {
         var result = Solution.Task2(FileReader.GetFileForDay(1, "testInput-part2.txt"));
-        Console.Out.WriteLine(result);
-        Assert.That(result == 281);
+        Assert.AreEqual(result, 281);
     }
 
     [Test]
     public void Task2_RealInput() {
         var result = Solution.Task2(FileReader.GetInputForDay(1));
-        Console.Out.WriteLine(result);
-        Assert.That(result == 54087);
+        Assert.AreEqual(result, 54087);
     }
 }
