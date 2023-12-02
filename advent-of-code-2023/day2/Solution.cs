1ab9a1bf94de9cedc57ab8a48210cab815cc37fb
Christoph Martenka <christoph.martenka@yahoo.de>
2023-12-02 09:56:49 +0100

    :white_check_mark: Day 2 Solution

---
+++ advent-of-code-2023/day2/Solution.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.RegularExpressions;
+
+namespace advent.of.code.day2;
+
+internal static class Solution {
+
+    internal static int Task1(StreamReader reader) {
+        var total = 0;
+        while (!reader.EndOfStream) {
+            var line = reader.ReadLine() ?? throw new Exception();
+
+            var regex = new Regex(@"^Game\s(?<id>\d+):((\s(?<cube>\d+\s(red|green|blue)),?)+;?)+$");
+            var match = regex.Match(line);
+
+            var cubes = match.Groups["cube"].Captures.Select(capture => {
+                var cube = capture.Value.Split(' ');
+                return (count: int.Parse(cube[0]), color: cube[1]);
+            });
+
+            var notPossible = cubes.Any(cube => cube.color switch {
+                    "red" => cube.count > 12,
+                    "green" => cube.count > 13,
+                    "blue" => cube.count > 14,
+                    _ => true
+                }
+            );
+            
+            if (notPossible) continue;
+            total += int.Parse(match.Groups["id"].Value);
+        }
+
+        return total;
+    }
+
+    internal static int Task2(StreamReader reader) {
+        var total = 0;
+        while (!reader.EndOfStream) {
+            var line = reader.ReadLine() ?? throw new Exception();
+
+            var regex = new Regex(@"^Game\s\d+:((\s(?<cube>\d+\s(red|green|blue)),?)+;?)+$");
+            var match = regex.Match(line);
+
+            var cubes = match.Groups["cube"].Captures.Select(capture => {
+                var cube = capture.Value.Split(' ');
+                return (count: int.Parse(cube[0]), color: cube[1]);
+            });
+
+            var minRed = 0;
+            var minGreen = 0;
+            var minBlue = 0;
+
+            foreach (var cube in cubes) {
+                switch (cube.color) {
+                    case "red":
+                        minRed = cube.count > minRed ? cube.count : minRed;
+                        break;
+                    case "green":
+                        minGreen = cube.count > minGreen ? cube.count : minGreen;
+                        break;
+                    case "blue":
+                        minBlue = cube.count > minBlue ? cube.count : minBlue;
+                        break;
+                }
+            }
+
+            total += minRed * minGreen * minBlue;
+        }
+
+        return total;
+    }
+}
---
+++ advent-of-code-2023/day2/Test.cs
@@ -0,0 +1,32 @@
+﻿using advent.of.code.util;
+using NUnit.Framework;
+
+namespace advent.of.code.day2;
+
+[TestFixture]
+internal class Test {
+
+    [Test]
+    public void Task1_TestInput() {
+        var result = Solution.Task1(FileReader.GetFileForDay(2, "testInput.txt"));
+        Assert.AreEqual(result, 8);
+    }
+
+    [Test]
+    public void Task1_RealInput() {
+        var result = Solution.Task1(FileReader.GetInputForDay(2));
+        Assert.AreEqual(result, 2505);
+    }
+
+    [Test]
+    public void Task2_TestInput() {
+        var result = Solution.Task2(FileReader.GetFileForDay(2, "testInput.txt"));
+        Assert.AreEqual(result, 2286);
+    }
+
+    [Test]
+    public void Task2_RealInput() {
+        var result = Solution.Task2(FileReader.GetInputForDay(2));
+        Assert.AreEqual(result, 70265);
+    }
+}
