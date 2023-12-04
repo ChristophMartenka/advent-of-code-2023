2e863a9748b00be5053fcb02cca45d029df4be1e
Christoph Martenka <christoph.martenka@yahoo.de>
2023-12-04 18:20:40 +0100

    :white_check_mark: Add Day 4 Solution

---
+++ advent-of-code-2023/day4/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+
+namespace advent.of.code.day4;
+
+internal static partial class Solution {
+
+    [GeneratedRegex(@".*\s(?<id>\d+)[:](\s*(?<winning>\d+)\s*)+[|](\s*(?<number>\d+)\s*)+")]
+    private static partial Regex ScratchCardRegex();
+
+    internal static int Task1(StreamReader reader) {
+        var total = 0;
+        while (!reader.EndOfStream) {
+            var line = reader.ReadLine() ?? throw new Exception();
+
+            var match = ScratchCardRegex().Match(line);
+            var winningNumbers = match.Groups["winning"].Captures.Select(c => int.Parse(c.Value));
+            var numbers = match.Groups["number"].Captures.Select(c => int.Parse(c.Value));
+
+            var matches = winningNumbers.Intersect(numbers).Count();
+            var sum = 1;
+
+            for (var i = 1; i < matches; i++) {
+                sum *= 2;
+            }
+
+            if (matches > 0) {
+                total += sum;
+            }
+        }
+
+        return total;
+    }
+
+    internal static int Task2(StreamReader reader) {
+        var copies = new Dictionary<int, int>();
+        while (!reader.EndOfStream) {
+            var line = reader.ReadLine() ?? throw new Exception();
+
+            var match = ScratchCardRegex().Match(line);
+            var id = int.Parse(match.Groups["id"].Value);
+            var winningNumbers = match.Groups["winning"].Captures.Select(c => int.Parse(c.Value));
+            var numbers = match.Groups["number"].Captures.Select(c => int.Parse(c.Value));
+
+            var matches = winningNumbers.Intersect(numbers).Count();
+
+            copies.TryAdd(id, 0);
+            copies[id] += 1;
+
+            for (var i = 1; i <= matches; i++) {
+                copies.TryAdd(id + i, 0);
+                copies[id + i] += 1 * copies[id];
+            }
+        }
+
+        return copies.Values.Sum();
+    }
+}
---
+++ advent-of-code-2023/day4/Test.cs
@@ -0,0 +1,22 @@
+﻿using advent.of.code.util;
+using NUnit.Framework;
+
+namespace advent.of.code.day4;
+
+[TestFixture]
+internal class Test {
+
+    private const int Day = 4;
+    
+    [TestCase(Day, "testInput.txt", TestName = "Day {0} Part 1 should be successful with test input", ExpectedResult = 13)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 1 should be successful with real input", ExpectedResult = 20407)]
+    public int Task1_Test(int day, string fileName) {
+        return Solution.Task1(FileReader.GetFileForDay(day, fileName));
+    }
+    
+    [TestCase(Day, "testInput.txt", TestName = "Day {0} Part 2 should be successful with test input", ExpectedResult = 30)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 2 should be successful with real input", ExpectedResult = 23806951)]
+    public int Task2_Test(int day, string fileName) {
+        return Solution.Task2(FileReader.GetFileForDay(day, fileName));
+    }
+}
