55cde2f02c8d1551d5ff8cb6ee4538a5bf6e9c58
Christoph Martenka <christoph.martenka@yahoo.de>
2023-12-06 11:36:00 +0100

    :white_check_mark: Day 5 Solution

---
+++ advent-of-code-2023/day6/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace advent.of.code.day6; 
+
+internal static class Solution {
+
+    internal static int Task1(StreamReader reader) {
+        var line = reader.ReadLine() ?? throw new Exception();
+        var times = line.Split(":")[1].Split(" ").Where(n => n != "").Select(int.Parse).ToArray();
+            
+        line = reader.ReadLine() ?? throw new Exception();
+        var distancesToBeat = line.Split(":")[1].Split(" ").Where(n => n != "").Select(int.Parse).ToArray();
+        
+        var total = 1;
+        
+        for (var i = 0; i < times.Length; i++) {
+            total *= GetPossibilitiesToBeat(times[i], distancesToBeat[i]);
+        }
+        
+        return total;
+    }
+
+    internal static long Task2(StreamReader reader) {
+        var line = reader.ReadLine() ?? throw new Exception();
+        var time = long.Parse(line.Split(":")[1].Replace(" ", ""));
+            
+        line = reader.ReadLine() ?? throw new Exception();
+        var distanceToBeat = long.Parse(line.Split(":")[1].Replace(" ", ""));
+        
+        return GetPossibilitiesToBeat(time, distanceToBeat);
+    }
+
+    private static int GetPossibilitiesToBeat(long remainingTime, long distanceToBeat) {
+        var possibilitiesToBeat = 0;
+
+        for (var time = 0; time < remainingTime; time++) {
+            if (time * (remainingTime - time) > distanceToBeat) {
+                possibilitiesToBeat++;
+            }
+        }
+
+        return possibilitiesToBeat;
+    }
+}
---
+++ advent-of-code-2023/day6/Test.cs
@@ -0,0 +1,22 @@
+﻿using advent.of.code.util;
+using NUnit.Framework;
+
+namespace advent.of.code.day6;
+
+[TestFixture]
+internal class Test {
+
+    private const int Day = 6;
+
+    [TestCase(Day, "testInput.txt", TestName = "Day {0} Part 1 should be successful with test input", ExpectedResult = 288)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 1 should be successful with real input", ExpectedResult = 32076)]
+    public int Task1_Test(int day, string fileName) {
+        return Solution.Task1(FileReader.GetFileForDay(day, fileName));
+    }
+
+    [TestCase(Day, "testInput.txt", TestName = "Day {0} Part 2 should be successful with test input", ExpectedResult = 71503)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 2 should be successful with real input", ExpectedResult = 34278221)]
+    public long Task2_Test(int day, string fileName) {
+        return Solution.Task2(FileReader.GetFileForDay(day, fileName));
+    }
+}
