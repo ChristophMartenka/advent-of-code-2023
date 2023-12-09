eac83fa9b3ce34c12a29745edf3dfced514bf808
Christoph Martenka <christoph.martenka@yahoo.de>
2023-12-09 14:28:38 +0100

    :white_check_mark: Day 9 Solution

---
+++ advent-of-code-2023/day9/Solution.cs
@@ -0,0 +1,64 @@
+﻿namespace advent.of.code.day9; 
+
+internal static class Solution {
+
+    internal static int Task1(StreamReader reader) {
+        var total = 0;
+        while (!reader.EndOfStream) {
+            var list = ReadDifferenceLists(reader);
+            for (var i = 0; i < list.Count; i++) {
+                var l = list[i];
+                if (i == 0) {
+                    l.Add(0);
+                } else {
+                    l.Add(l.Last() + list[i - 1].Last());
+                }
+            }
+
+            total += list.Last().Last();
+        }
+
+        return total;
+    }
+
+    internal static int Task2(StreamReader reader) {
+        var total = 0;
+        while (!reader.EndOfStream) {
+            var list = ReadDifferenceLists(reader);
+            for (var i = 0; i < list.Count; i++) {
+                var l = list[i];
+                if (i == 0) {
+                    l.Insert(0, 0);
+                } else {
+                    l.Insert(0, l.First() - list[i - 1].First());
+                }
+            }
+
+            total += list.Last().First();
+        }
+
+        return total;
+    }
+    
+    private static List<List<int>> ReadDifferenceLists(TextReader reader) {
+        var line = reader.ReadLine() ?? throw new Exception();
+
+        var numbers = line.Split(" ").Select(int.Parse).ToList();
+            
+        var list = new List<List<int>> { numbers };
+
+        var curList = numbers;
+        while (curList.Any(n => n != 0)) {
+            var newList = new List<int>();
+            for (var i = 0; i < curList.Count - 1; i++) {
+                newList.Add(curList[i + 1] - curList[i]);
+            }
+            list.Add(newList);
+            curList = newList;
+        }
+
+        list.Reverse();
+
+        return list;
+    }
+}
---
+++ advent-of-code-2023/day9/Test.cs
@@ -0,0 +1,22 @@
+﻿using advent.of.code.util;
+using NUnit.Framework;
+
+namespace advent.of.code.day9;
+
+[TestFixture]
+internal class Test {
+
+    private const int Day = 9;
+
+    [TestCase(Day, "testInput.txt", TestName = "Day {0} Part 1 should be successful with test input", ExpectedResult = 114)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 1 should be successful with real input", ExpectedResult = 1861775706)]
+    public int Task1_Test(int day, string fileName) {
+        return Solution.Task1(FileReader.GetFileForDay(day, fileName));
+    }
+
+    [TestCase(Day, "testInput.txt", TestName = "Day {0} Part 2 should be successful with test input", ExpectedResult = 2)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 2 should be successful with real input", ExpectedResult = 1082)]
+    public int Task2_Test(int day, string fileName) {
+        return Solution.Task2(FileReader.GetFileForDay(day, fileName));
+    }
+}
