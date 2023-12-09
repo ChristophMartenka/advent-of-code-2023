f5bc0ceae3eb8b02c93a2279164ee240bea9d6ff
Christoph Martenka <christoph.martenka@yahoo.de>
2023-12-09 14:28:01 +0100

    :white_check_mark: Day 8 Solution

---
+++ advent-of-code-2023/day8/Solution.cs
@@ -0,0 +1,74 @@
+﻿using System.Text.RegularExpressions;
+
+namespace advent.of.code.day8; 
+
+internal static partial class Solution {
+
+    [GeneratedRegex(@"(?<key>[0-9A-Z]{3})\s=\s\((?<left>[0-9A-Z]{3}),\s(?<right>[0-9A-Z]{3})\)")]
+    private static partial Regex DirectionMapRegex();
+
+    internal static long Task1(StreamReader reader) {
+        Read(reader, out var directions, out var left, out var right);
+        
+        return CountSteps("AAA", directions, left, right);
+    }
+
+    internal static long Task2(StreamReader reader) {
+        var startingNodes = Read(reader, out var directions, out var left, out var right);
+        
+        return startingNodes.AsParallel()
+            // count steps for each starting node to get to a node ending with Z
+            .Select(pos => CountSteps(pos, directions, left, right))
+            // get lowest common multiple of all cycles
+            .Aggregate((cycleA, cycleB) => cycleA * cycleB / GreatestCommonDelimiter(cycleA, cycleB));
+    }
+
+    private static List<string> Read(StreamReader reader, out char[] directions, out Dictionary<string, string> left, out Dictionary<string, string> right) {
+        var directionsLine = reader.ReadLine() ?? throw new Exception();
+        directions = directionsLine.ToCharArray();
+
+        // empty line
+        reader.ReadLine();
+        
+        left = new Dictionary<string, string>();
+        right = new Dictionary<string, string>();
+
+        var startingNodes = new List<string>();
+        
+        while (!reader.EndOfStream) {
+            var line = reader.ReadLine() ?? throw new Exception();
+            
+            var match = DirectionMapRegex().Match(line);
+
+            var key = match.Groups["key"].Value;
+            
+            left.Add(key, match.Groups["left"].Value);
+            right.Add(key, match.Groups["right"].Value);
+
+            if (key.EndsWith("A")) startingNodes.Add(key);
+        }
+
+        return startingNodes;
+    }
+
+    private static long CountSteps(string pos, IReadOnlyList<char> directions, IReadOnlyDictionary<string, string> left, IReadOnlyDictionary<string, string> right) {
+        var currentDirection = 0;
+        var steps = 0L;
+        while (!pos.EndsWith("Z")) {
+            steps++;
+            pos = directions[currentDirection] == 'L' ? left[pos] : right[pos];
+            
+            currentDirection++;
+            if (currentDirection >= directions.Count) {
+                currentDirection = 0;
+            }
+        }
+
+        return steps;
+    }
+
+    private static long GreatestCommonDelimiter(long a, long b) {
+        if (b == 0) return a;
+        return GreatestCommonDelimiter(b, a % b);
+    }
+}
---
+++ advent-of-code-2023/day8/Test.cs
@@ -0,0 +1,23 @@
+﻿using advent.of.code.util;
+using NUnit.Framework;
+
+namespace advent.of.code.day8;
+
+[TestFixture]
+internal class Test {
+
+    private const int Day = 8;
+
+    [TestCase(Day, "testInput.txt", TestName = "Day {0} Part 1 should be successful with test input", ExpectedResult = 2)]
+    [TestCase(Day, "testInput-2.txt", TestName = "Day {0} Part 1 should be successful with second test input", ExpectedResult = 6)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 1 should be successful with real input", ExpectedResult = 13771)]
+    public long Task1_Test(int day, string fileName) {
+        return Solution.Task1(FileReader.GetFileForDay(day, fileName));
+    }
+
+    [TestCase(Day, "testInput-part2.txt", TestName = "Day {0} Part 2 should be successful with test input", ExpectedResult = 6)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 2 should be successful with real input", ExpectedResult = 13129439557681)]
+    public long Task2_Test(int day, string fileName) {
+        return Solution.Task2(FileReader.GetFileForDay(day, fileName));
+    }
+}
